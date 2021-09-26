ed889903e7f8d51729e688dd049b3bbc132eb2f7
glsm4tt <appstrophe@gmail.com>
2021-09-26 20:24:30 +0200

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
