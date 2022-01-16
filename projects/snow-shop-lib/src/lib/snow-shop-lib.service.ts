bb848c79721bda91cadb75aa5ef76e17997591cb
sdedieu <dedieu.sylvain38@gmail.com>
2022-01-16 18:33:54 +0100

    change to cart item count behaviour subject

--- projects/snow-shop-lib/src/lib/snow-shop-lib.service.ts
+++ projects/snow-shop-lib/src/lib/snow-shop-lib.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnowShopLibService {
 
+  cartItemCount$ = new BehaviorSubject(0);
+
   constructor() { }
+
+  getCartItemCount(): Observable<number> {
+    return this.cartItemCount$.asObservable();
+  }
+
+  increment(): void {
+    this.cartItemCount$.next(this.cartItemCount$.value + 1);
+  }
 }
